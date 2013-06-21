4c8514a465d011ff6b4c7077c4f0eac47838b38a
aravind <aravind.soundararajan@chronus.com>
2013-06-21 10:49:07 +0530

    profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,6 +30,17 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 		@title = "Edit user"
   end
+  def update
+		@user = User.find(params[:id])
+		if @user.update_attributes(params[:user])
+			flash[:success] = "Profile updated."
+			redirect_to @user
+		else
+			@title = "Edit user"
+			render 'edit'
+		end
+  end	
+
   private
 		def authenticate
 			deny_access unless signed_in?
